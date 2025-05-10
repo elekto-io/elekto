c45c13f09b81460bd18b2059a26f6184dd39b03c
Ludo van Orden <ludo@vanorden.eu>
2025-05-10 14:56:29 +0200

    OPT: improved test coverage of elekto/core module

--- test/conftest.py
+++ test/conftest.py
@@ -3,11 +3,14 @@ import os
 import pytest
 
 # Inject required environment variables
-# TODO: make this nicer; this injection must happen before the APP and SESSION imports
+# TODO: make this nicer; this injection must happen before the APP and SESSION imports (or any other imports that
+#  trigger APP and SESSION).
 os.environ['DB_CONNECTION'] = 'sqlite'
 
 from elekto import APP, SESSION
 from elekto.models.sql import drop_all, migrate
+
+from test.factories import ElectionFactory, BallotFactory
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -28,3 +31,14 @@ def client():
 @pytest.fixture()
 def salt() -> bytes:
     return os.urandom(16)
+
+
+@pytest.fixture()
+def db_session():
+    return SESSION
+
+
+@pytest.fixture(autouse=True)
+def db_factories(db_session):
+    ElectionFactory._meta.sqlalchemy_session = db_session
+    BallotFactory._meta.sqlalchemy_session = db_session
