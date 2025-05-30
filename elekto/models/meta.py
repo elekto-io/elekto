c46893b3b0146284c37bdfdb9602a45b947af7cb
Ludo van Orden <ludo@vanorden.eu>
2025-05-30 08:33:53 +0200

    OPT: added tests for models

--- elekto/models/meta.py
+++ elekto/models/meta.py
@@ -136,6 +136,8 @@ class Election(Meta):
         return utils.parse_yaml(os.path.join(self.path, Election.VOT))
         
     def showfields(self):
+        # FIXME: show_candidate_fields could be None (as is the case in the name_the_app example meta), leading to anAdd commentMore actions
+        #  error if showfields() is called.
         return dict.fromkeys(self.election['show_candidate_fields'], '')
 
     def candidates(self):
